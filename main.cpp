c2141a7eb407e3d36a8e7ea949dfceb821a9a88b
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2025-03-06 02:37:30 -0500

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include "cpu_arch.hpp"
+#include <unordered_map>
+
+int main() {
+	std::unordered_map<std::string, std::string> test_map{};
+	test_map.reserve(354);
+	std::cout << "CPU ARCH INDEX: " << oiml::cpu_arch_index << std::endl;
+	impl_test_function_01(oiml::cpu_arch_index);
+	return 0;
+}
